06f95020dbd60443310865da5861d41d31cdeea1
FINEP UTFPR <jeanbrandaocarvalho@hotmail.com>
2024-05-20 16:09:31 -0300

    New map in home page

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,9 +1,17 @@
 
+
+
+
 import { Link } from "react-router-dom";
+
+const states = ["São Paulo", "Espírito Santo", "Minas Gerais", "Rio de Janeiro", "Paraná"];
+const totalDistance = 2773; // em quilômetros
+const totalHours = 40;
+const totalGigabytes = 25;
 
 const About = () => {
     return (
-<body className="bg-zinc-900 h-screen">
+<body className="bg-zinc-900 h-screen ">
 <header className="bg-zinc-900 flex  px-3 ">
 <h1 className="text-4xl font-semibold italic mt-1  mr-7 text-yellow-400 text-left"> CarCara</h1>
       <Link to={"/"}> 
@@ -13,7 +21,7 @@ const About = () => {
         <p className="text-xl font-semibold  ml-5  mb-11 mt-3 text-neutral-400 rounded relative hover:scale-110 duration-200">About</p>
       </Link> 
  </header>
- <div className="flex items-center justify-center mt-11">
+ <div className="flex items-center justify-center mt-11 mr-7 ml-7">
     <div className="max-w-3xl px-2 text-center text-white mb-11 mt-11">
         <h1 className="text-5xl font-bold mb-4 text-roboto mt-11 text-yellow-400">About us</h1>
         
@@ -24,6 +32,10 @@ const About = () => {
         <p className="text-base md:text-sm mb-8 text-roboto mt-4 text-yellow-100">Our website provides researchers with comprehensive data, including videos, measurements, and vehicle locations. Additionally, it facilitates data visualization and extraction for research in computer vision, map creation, and the calibration of sensors and radar systems. Join us in exploring the future of vehicle automation.</p>
 
 
+       
+                <p className="text-gray-400 mt-11">Total Distance: {totalDistance} km</p>
+                <p className="text-gray-400">Total Hours Recorded: {totalHours} hours</p>
+                <p className="text-gray-400">Total Terabytes Used: {totalGigabytes} TB</p>
 
     </div>
 </div>
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,38 +1,61 @@
-
 import { Link } from "react-router-dom";
 
+import  MapComponent  from "../Classes/MapComponent"
+
 const Video = () => {
+    // Dados estáticos dos estados percorridos e outras informações
+    const states = ["São Paulo", "Espírito Santo", "Minas Gerais", "Rio de Janeiro", "Paraná"];
+    const totalDistance = 2773; // em quilômetros
+    const totalHours = 40;
+    const totalGigabytes = 25;
+
     return (
-<body className="bg-zinc-900  h-screen">
-<header className="bg-zinc-900 flex  px-3 ">
-     <h1 className="text-4xl font-semibold italic mt-1  mr-7 text-yellow-400 text-left"> CarCara</h1>
-      <Link to={"/"}> 
-        <p className="text-xl font-semibold  ml-11 mr-5 mb-4 mt-3 text-neutral-400      rounded relative hover:scale-110 duration-200">Home</p>
-      </Link>
-      <Link to={"/About"}> 
-        <p className="text-xl font-semibold  ml-5  mb-11 mt-3 text-neutral-400 rounded relative hover:scale-110 duration-200">About</p>
-      </Link> 
- </header>
-    <div className="flex items-center justify-center mt-11" > 
-     <div className="text-center text-white mb-11 mt-11">
-        <h1 className="text-5xl font-bold mb-4 mt-11 text-yellow-300 text-roboto">Vehicle Acquisitions</h1>
-        <p className="text-lg mb-8 text-roboto">Empower your vision with data</p>
+        <body className="bg-zinc-900 h-screen">
+            <header className="flex px-3">
+                <h1 className="text-4xl font-semibold italic mt-1 mr-7 text-yellow-400 text-left">CarCara</h1>
+                <Link to={"/"}>
+                    <p className="text-xl font-semibold ml-11 mr-5 mb-4 mt-3 text-neutral-400 rounded relative hover:scale-110 duration-200">Home</p>
+                </Link>
+                <Link to={"/About"}>
+                    <p className="text-xl font-semibold ml-5 mb-11 mt-3 text-neutral-400 rounded relative hover:scale-110 duration-200">About</p>
+                </Link>
+            </header>
+            <div className="flex items-center justify-center ">
+                <div className="text-center text-white mb-11 ml-7 mr-7">
+                    <h1 className="text-5xl font-bold mb-4 mt-11 text-yellow-300 text-roboto">Vehicle Acquisitions</h1>
+                    <p className="text-lg mb-8 text-roboto">Empower your vision with data</p>
 
-        <Link to={"/Acquisitions"}> 
-        <p className="bg-yellow-300 text-zinc-900 hover:bg-yellow-100 text-lg ml-7 mr-7 font-bold py-2 px-6 rounded-full transition duration-300 text-roboto">Explore Acquisitions  </p>
-        </Link> 
-
-
-
-
-      
-     </div>
-    </div>
-    
-      </body>
-      )
+                    <Link to={"/Acquisitions"}>
+                        <p className="bg-yellow-300 text-zinc-900 hover:bg-yellow-100 text-lg  font-bold py-2 px-6 rounded-full transition duration-300 text-roboto">Explore Acquisitions</p>
+                    </Link>
+                       
+                </div>
+            </div>
 
 
+            <div className="  text-center ml-11 mr-11">
+                <div>
+                <MapComponent states={states} />
 
+                </div>
+
+
+           
+            
+
+             <p className="text-yellow-300 mt-3">States visited: 
+             <span className="text-gray-300">
+             { " " + states.join(', ')}
+             </span>
+             </p>
+            
+
+
+            </div>
+
+            
+        </body>
+    )
 }
+
 export default Video;
