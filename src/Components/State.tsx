217ee6e0c50ee853372311e66f774788cad786f5
FINEP UTFPR <jeanbrandaocarvalho@hotmail.com>
2024-05-28 19:08:27 -0300

    New combobox for state page (sort by alphabetical or time recorded)

--- src/Components/State.tsx
+++ src/Components/State.tsx
@@ -1,174 +1,172 @@
-import { useEffect, useState } from 'react';
-import  api  from '../Services/api';
+import React, { useEffect, useState } from 'react';
+import api from '../Services/api';
 import formatDuration from '../Classes/DateTimeFormat';
-import { Link } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from 'react-router-dom';
 import Select from 'react-select';
-import carcara from "../Components/img/carcara23.png";
-import  customStyles   from '../Styles/Header.tsx'
-import MapState from '../Classes/MapState'
+import MapState from '../Classes/MapState';
+import carcara from '../Components/img/carcara23.png';
+import customStyles from '../Styles/Header.tsx';
 
+
+interface FilesProps {
+  id: string;
+  TimeStemp: Date;
+  Videoname: string;
+  Bairro: string;
+  Cidade: string;
+  Estado: string;
+  Link: string;
+  Gps_y: number;
+  Gps_X: number;
+  Gps_Z: number;
+}
 
 const options = [
   { value: '/', label: 'Home' },
   { value: '/About', label: 'About' }
 ];
-const handleChange = (newValue: unknown) => {
-  const selectedOption = newValue as { value: string; label: string; } | null;
-        if (selectedOption !== null && 'value' in selectedOption) {
-            // Alterar o estado do histórico do navegador para navegar para o URL da opção selecionada
-            history.pushState(null, '', selectedOption.value);
-            // Disparar o evento popstate para garantir que os manipuladores de eventos do histórico sejam chamados
-            window.dispatchEvent(new PopStateEvent('popstate', { state: null }));
-        }
-};
-
-interface FilesProps {
-  id   : string ;
-  TimeStemp : Date;
-  Videoname : string;
-  Bairro :string;
-  Cidade :string;
-  Estado :string;
-  Link :string;
-  Gps_y :number;
-  Gps_X :number;
-  Gps_Z :number;
-}
-
 
 const State = () => {
-
   const { state } = useParams();
-   
   const [filesdata, setFiles] = useState<FilesProps[]>([]);
- 
+  const [sortBy, setSortBy] = useState<'timeRecorded' | 'city'>('timeRecorded'); // Estado para controlar a opção selecionada
+
   useEffect(() => {
     loadFiles();
   }, []);
 
   async function loadFiles() {
-    const response = await api.get("/videofiles");
+    const response = await api.get('/videofiles');
     setFiles(response.data);
   }
 
   function getCityData(stateclicked: string) {
-
-     const filteredData = filesdata.filter((item) => item.Estado === stateclicked);
-     const cidadesFiltradas = filteredData.map((item) => item.Cidade);
-     const cidadesFiltradasSemDuplicatas = cidadesFiltradas.filter((cidade, index, array) => {
-      return array.indexOf(cidade) === index;
-  });
-  const cidadesFiltradasOrdenadas = cidadesFiltradasSemDuplicatas.sort((a, b) => {
-    return a.localeCompare(b);
-});
-
-
-     console.log(cidadesFiltradasOrdenadas);
-     return cidadesFiltradasOrdenadas;
-   
+    const filteredData = filesdata.filter((item) => item.Estado === stateclicked);
+    const uniqueCities = [...new Set(filteredData.map((item) => item.Cidade))];
+    return uniqueCities.sort(); // Ordenando alfabeticamente
   }
 
-
-  
   function countDocumentsByCity(city: string): number {
-    return filesdata.filter(file => file.Cidade === city).length*300;
-   
+    return filesdata.filter((file) => file.Cidade === city).length * 300;
   }
 
-
-
-
-
-  
   const uniqueCities = state ? getCityData(state) : [];
 
-  
+  const handleChange = (newValue: unknown) => {
+    if (newValue !== null && typeof newValue === 'object') {
+      const selectedOption = newValue as { value: string; label: string };
+      setSortBy(selectedOption.value as 'timeRecorded' | 'city');
+    }
+  };
 
-  window.onload = function() {
-    window.scrollTo(0, 0);
-}
+  const handleChangeHome = (newValue: unknown) => {
+    const selectedOption = newValue as { value: string; label: string; } | null;
+    if (selectedOption !== null && 'value' in selectedOption) {
+        // Alterar o estado do histórico do navegador para navegar para o URL da opção selecionada
+        history.pushState(null, '', selectedOption.value);
+        // Disparar o evento popstate para garantir que os manipuladores de eventos do histórico sejam chamados
+        window.dispatchEvent(new PopStateEvent('popstate', { state: null }));
+    }
+  };
+
+  const sortedCities = [...uniqueCities].sort((a, b) => {
+    if (sortBy === 'city') {
+      return a.localeCompare(b); // Ordenar por ordem alfabética
+    } else {
+      return countDocumentsByCity(b) - countDocumentsByCity(a); // Ordenar pelo tempo gravado em ordem decrescente
+    }
+  });
 
   return (
-    <body className="bg-zinc-950  h-screen">
-  
-  
-  <header className="flex px-3">
-                <img
-                    src={carcara}
-                    alt="Descrição da imagem"
-                    className="mr-2 mt-2"
-                    width="250"
-                    style={{ height: "40px" }}
-                />
-                <div className="flex items-center mt-2">
-                
-                    <Select
-                        options={options}                 
-                        styles={customStyles}
-                        placeholder="Home"
-                        className="mr-5 font-bold"
-                        classNamePrefix='Select'
-                        onChange={handleChange} 
-                              
-                    />
-                </div>
-            </header>
-    <div className="bg-zinc-900  ">
+    <body className="bg-zinc-950 h-screen">
+      <header className="flex px-3">
+        <img
+          src={carcara}
+          alt="Descrição da imagem"
+          className="mr-2 mt-2"
+          width="250"
+          style={{ height: '40px' }}
+        />
+        <div className="flex items-center mt-2">
+          <Select
+            options={options}                 
+            styles={customStyles}
+            placeholder="Home"
+            className="mr-5 font-bold"
+            classNamePrefix='Select'
+            onChange={handleChangeHome} 
+          />
+        </div>
+      </header>
+      <div className="bg-zinc-900 ">
+        <div className="bg-zinc-950 flex justify-center px-4 ">
+          <main className="my-5 w-full md:max-w-2xl h-full">
+            <div className="text-left">
+              <h1 className="text-4xl font-medium mb-4 text-orange-100 text-left text-roboto">
+                Changing of <br /> Acquired <span className="font-medium text-orange-300 text-roboto">Cities</span>
+              </h1>
+            </div>
+            <MapState estadoSelecionado={state} />
 
-   
+            <div className='flex'>
 
-    <div className="bg-zinc-950 flex justify-center px-4  ">
-      <main className="my-5 w-full md:max-w-2xl h-full">
-      <div className="text-left">
-      <h1 className="text-4xl font-medium mb-4 text-orange-100 text-left  text-roboto">
-      Changing of <br /> Acquired <span className='font-medium text-orange-300 text-roboto' > Cities</span>
-      </h1>
-      
-    </div>
-    <MapState estadoSelecionado={state} />
-    <h2 className="text-2xl font-normal font-siz  text-yellow-300 text-left mt-3 text-roboto">
+            <div className='mt-5 mb-2 ml-1'>              
+            <Select
+            styles={customStyles}
+            placeholder="Time recorded"
+                options={[
+                  { value: 'timeRecorded', label: 'Time recorded' },
+                  { value: 'city', label: 'City' },
+                ]}
+                defaultValue={{ value: 'timeRecorded', label: 'Order by:' }}         
+                className="mr-5 font-bold text-white"
+                classNamePrefix="Select"
+                onChange={handleChange}
+              />
+            </div>
 
-    <p>
-            <span className='text-gray-400 font-normal'>
-            State:
 
-            </span>
-            <span className='text-yellow-300 font-semibold'>
-            {" "+ state} 
-            </span>
-         
-          </p>
-      
-      </h2>
-        <section className="grid grid-cols-2 gap-4 w-full mt-3">
-          {uniqueCities.map((cities) => {
-            return (
-              cities !== "Not Found" && (
-                <Link to={`/city/${cities}`} key={cities}>
-                <article className="bg-zinc-800 rounded p-2 relative hover:scale-105 duration-200 h-full">
-                   <p>
-                    <span className="font-medium text-neutral-300 text-xl text-roboto">City: </span>
-                    <span className="font-medium text-orange-300 text-xl text-roboto">{cities} </span>
-                  </p>
 
-                  <p>
-                    <span className="font-medium text-neutral-300 text-roboto">Time recorded: </span>
-                    <span className="font-medium text-green-200 text-roboto"> {formatDuration(countDocumentsByCity(cities)) }  </span>
-                   
-                  </p>
-                </article>
+            <h2 className="text-2xl font-normal font-siz text-yellow-300 text-left mt-5 text-roboto">
+              <p>
+                <span className="text-gray-400 font-normal">State:</span>
+                <span className="text-orange-300 font-semibold"> {state} </span>
+              </p>             
+            </h2>
+           
+
+
+           
+            </div>
+           
+            <div className='text-white'>
+
+            </div>
+           
+            <section className="grid grid-cols-2 gap-4 w-full mt-3">
+              {sortedCities.map((city) => (
+                <Link to={`/city/${city}`} key={city}>
+                  <article className="bg-zinc-800 rounded p-2 relative hover:scale-105 duration-200 h-full">
+                    <p>
+                      <span className="font-medium text-neutral-300 text-xl text-roboto">City: </span>
+                      <span className="font-medium text-orange-300 text-xl text-roboto">{city} </span>
+                    </p>
+                    <p>
+                      <span className="font-medium text-neutral-300 text-roboto">Time recorded: </span>
+                      <span className="font-medium text-green-200 text-roboto">
+                        {' '}
+                        {formatDuration(countDocumentsByCity(city))}{' '}
+                      </span>
+                    </p>
+                  </article>
                 </Link>
-              ) 
-            );
-          })}
-        </section>
-      </main>
-    </div>
-    </div>
+              ))}
+            </section>
+          </main>
+        </div>
+      </div>
     </body>
-      
-  )
-}
+  );
+};
 
 export default State;
--- src/Styles/Header.tsx
+++ src/Styles/Header.tsx
@@ -1,30 +1,33 @@
-import { StylesConfig } from 'react-select';
-import { CSSObject } from '@emotion/react';
+// Arquivo customStyles.js
 
-// Defina seus estilos personalizados
+import { StylesConfig } from 'react-select';
+
 const customStyles: StylesConfig = {
   control: (provided) => ({
     ...provided,
     backgroundColor: '#363636',
-    color: 'white'
+    color: 'white',
   }),
   menu: (provided) => ({
     ...provided,
     backgroundColor: '#363636',
     color: 'white'
   }),
-  option: (base, { isSelected, isFocused }) => {
-    const customOptionStyles: CSSObject = {
-      ...base,
-      backgroundColor: isSelected ? '#4a4a4a' : 'transparent', // Usando 'transparent' quando não selecionado
-      color: 'white', // Cor do texto do item
-      '&:hover': {
-        backgroundColor: isFocused ? 'yellow' : '#666666', // Cor de fundo quando o mouse está sobre o item
-        color: 'black' // Cor do texto do item quando o mouse está sobre ele
-      }
-    };
-    return customOptionStyles;
-  },
+  option: (base, { isSelected, isFocused }) => ({
+    ...base,
+    backgroundColor: isSelected ? '#4a4a4a' : 'transparent', // Usando 'transparent' quando não selecionado
+    color: 'white', // Cor do texto do item
+    '&:hover': {
+      backgroundColor: isFocused ? 'yellow' : '#666666', // Cor de fundo quando o mouse está sobre o item
+      color: 'black' // Cor do texto do item quando o mouse está sobre ele
+    }
+  }),
+  singleValue: (provided) => ({
+    ...provided,
+    color: 'lightgray', // Define a cor do texto para branco
+    fontWeight: 'bold', // Mantém o texto em negrito
+  }),
+  
   // Adicione mais estilos conforme necessário
 };
 
