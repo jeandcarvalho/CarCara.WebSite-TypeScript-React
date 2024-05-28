8e496c8084d792017a01842d8d0334188f841c6f
FINEP UTFPR <jeanbrandaocarvalho@hotmail.com>
2024-05-28 19:09:37 -0300

    New combobox for state page (sort by alphabetical or time recorded) fix react not used

--- src/Components/State.tsx
+++ src/Components/State.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import api from '../Services/api';
 import formatDuration from '../Classes/DateTimeFormat';
 import { Link, useParams } from 'react-router-dom';
