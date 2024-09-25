01c7721a395144cbd1f2b5521f5b49d53e2a9110
FINEP UTFPR <jeanbrandaocarvalho@hotmail.com>
2024-09-25 10:52:33 -0300

    updates in mobile design

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,19 +4,11 @@ const Footer: React.FC = () => (
   <footer className="bg-zinc-900 text-white py-4">
     <div className="container mx-auto text-center">
       <p className="text-sm px-2">&copy; 2024 GSA. All rights reserved.</p>
-      <nav className="mt-2">
-        <Link to={"/"}>
-          <a className="text-zinc-400 hover:text-white mx-2">Home</a>
-        </Link>
-        <Link to={"/Search"}>
-          <a className="text-zinc-400 hover:text-white mx-2">Acquisitions</a>
-        </Link>
-        <Link to={"/About"}>
-          <a className="text-zinc-400 hover:text-white mx-2">About</a>
-        </Link>
-        <Link to={"/OurModels"}>
-          <a href="/ourmodels" className="text-zinc-400 hover:text-white mx-2">Our Models</a>
-        </Link>
+      <nav className="flex justify-center mt-2 flex-wrap">
+        <Link to="/" className="text-zinc-400 hover:text-white mx-2">Home</Link>
+        <Link to="/Search" className="text-zinc-400 hover:text-white mx-2">Acquisitions</Link>
+        <Link to="/About" className="text-zinc-400 hover:text-white mx-2">About</Link>
+        <Link to="/OurModels" className="text-zinc-400 hover:text-white mx-2">Our Models</Link>
       </nav>
     </div>
   </footer>
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,5 +1,6 @@
-
+import React from 'react';
 import Select from 'react-select';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate instead
 import carcara from "../Components/img/carcara23.png";
 import customStyles from '../Styles/Header.tsx';
 
@@ -10,34 +11,38 @@ const options = [
   { value: '/OurModels', label: 'Our Models' },
 ];
 
-const handleChange = (newValue: unknown) => {
-  const selectedOption = newValue as { value: string; label: string; } | null;
-  if (selectedOption !== null && 'value' in selectedOption) {
-    history.pushState(null, '', selectedOption.value);
-    window.dispatchEvent(new PopStateEvent('popstate', { state: null }));
-  }
-};
+const Header: React.FC = () => {
+  const navigate = useNavigate(); // Use useNavigate to get the navigate function
 
-const Header: React.FC = () => (
-  <header className="flex px-3">
-    <img
-      src={carcara}
-      alt="Descrição da imagem"
-      className="mr-2 mt-2"
-      width="250"
-      style={{ height: "40px" }}
-    />
-    <div className="flex items-center">
-      <Select
-        options={options}
-        styles={customStyles}
-        placeholder="Home"
-        className="mr-5 font-bold p-2"
-        classNamePrefix='Select'
-        onChange={handleChange}
+  const handleChange = (newValue: unknown) => {
+    const selectedOption = newValue as { value: string; label: string; } | null;
+    if (selectedOption !== null && 'value' in selectedOption) {
+      navigate(selectedOption.value); // Use navigate to change routes
+      window.dispatchEvent(new PopStateEvent('popstate', { state: null }));
+    }
+  };
+
+  return (
+    <header className="flex flex-col md:flex-row items-center justify-between p-3 bg-zinc-950 shadow-md">
+      <img
+        src={carcara}
+        alt="Descrição da imagem"
+        className="mr-2 mb-2 md:mb-2"
+        width="250"
+        style={{ height: "40px" }}
       />
-    </div>
-  </header>
-);
+      <div className="flex items-center w-full md:w-auto">
+        <Select
+          options={options}
+          styles={customStyles}
+          placeholder="Home"
+          className="w-full md:w-auto mr-5 font-bold p-2"
+          classNamePrefix='Select'
+          onChange={handleChange}
+        />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
