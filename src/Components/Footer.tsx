a932cb917e056486a00a5c681d4fd0df773b7cbd
FINEP UTFPR <jeanbrandaocarvalho@hotmail.com>
2024-10-15 01:00:26 -0300

    New search and view all icons

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,17 +1,25 @@
-import { Link } from 'react-router-dom';
+
 
 const Footer: React.FC = () => (
-  <footer className="bg-zinc-900 text-white py-4">
+  <footer className="bg-zinc-900 text-white py-2">
     <div className="container mx-auto text-center">
       <p className="text-sm px-2">&copy; 2024 GSA. All rights reserved.</p>
-      <nav className="flex justify-center mt-2 flex-wrap">
-        <Link to="/" className="text-zinc-400 hover:text-white mx-2">Home</Link>
-        <Link to="/Search" className="text-zinc-400 hover:text-white mx-2">Acquisitions</Link>
-        <Link to="/About" className="text-zinc-400 hover:text-white mx-2">About</Link>
-        <Link to="/OurModels" className="text-zinc-400 hover:text-white mx-2">Our Models</Link>
-      </nav>
+     
     </div>
   </footer>
 );
 
 export default Footer;
+/*
+
+import { Link } from 'react-router-dom';
+ <nav className="flex justify-center mt-2 flex-wrap">
+        <Link to="/" className="text-zinc-400 hover:text-white mx-2">Home</Link>
+        <Link to="/Search" className="text-zinc-400 hover:text-white mx-2">Acquisitions</Link>
+        <Link to="/About" className="text-zinc-400 hover:text-white mx-2">About</Link>
+        <Link to="/OurModels" className="text-zinc-400 hover:text-white mx-2">Our Models</Link>
+      </nav>
+
+
+
+*/
