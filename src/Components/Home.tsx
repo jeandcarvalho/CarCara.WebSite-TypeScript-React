82c61e0fcbb27c96b4e422cf0a96e53fe975b202
FINEP UTFPR <jeanbrandaocarvalho@hotmail.com>
2025-02-11 01:15:37 -0300

    New Full acquisitions Route

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -116,7 +116,7 @@ const Home: React.FC = () => {
             <div className="bg-zinc-900 text-white py-12 px-4">
                 <div className="max-w-5xl mx-auto text-center">
                     <h2 className="text-3xl md:text-4xl font-bold mb-4 text-yellow-300 text-roboto">Examples</h2>
-                    <div className="grid grid-cols-3 md:grid-cols-3 gap-4">
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                         {/* Placeholder for the video thumbnails */}
                         <div className="flex justify-center">
                        
