f9383fb53b814f86c7e946e3160587431f01d073
Jean Carvalho <jeanbrandaocarvalho@hotmail.com>
2025-03-18 03:36:28 -0300

    Updates v4.2

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -28,8 +28,49 @@ const Home: React.FC = () => {
 
 
 
-              {/* CarCara-DAQ Test Section */}
-          <div className="flex-grow flex justify-center mt-3 px-4">
+           
+          
+          {/* Sessão CarCara-360DAQ */}
+<div className="bg-zinc-950 flex-grow flex justify-center px-4">
+    <div className="w-full max-w-5xl flex mt-3  flex-col md:flex-row items-center md:items-start px-3 text-white">
+
+        
+    <div className="  md:w-1/2 ml-3  text-center md:text-left flex justify-center md:justify-start md:items-center h-full">
+            <div className="flex flex-col items-center md:items-start">
+            <img
+    src={utfpr}
+    alt="Descrição da imagem"
+    width="280"
+    height="60"
+    className="inline-block mt-5 mb-2"
+  />
+
+                <h1 className="text-2xl md:text-3xl font-bold mb-4 text-yellow-300 text-roboto mr-3">
+                    CarCara-DAQ: A Multi-Sensor Dataset for Real-Time Automotive Data Acquisition
+                </h1>
+                <Link to="/Intermediary">
+                    <button className=" bg-yellow-300 text-zinc-900 hover:bg-yellow-100 text-base md:text-lg font-bold py-2 px-4 md:px-6 rounded-full transition duration-300 text-roboto mb-6">
+                        Download Here
+                    </button>
+                </Link>
+            </div>
+        </div>
+    <div className="md:w-1/2 mb-5 flex justify-center items-center mr-3">
+    <img 
+  className="w-full max-w-md md:max-w-lg h-[300px] md:h-[415px] object-cover"
+  src={heat} 
+  alt="Descrição do GIF"
+/>
+        </div>
+
+
+
+      
+    </div>
+</div>
+
+   {/* CarCara-DAQ Test Section */}
+   <div className="flex-grow flex justify-center mt-3 px-4">
               <div className="w-full max-w-5xl mb-3 flex flex-col md:flex-row items-center md:items-start px-3 text-white">
 
 
@@ -38,18 +79,11 @@ const Home: React.FC = () => {
                     
 
                      
-                      <img
-    src={utfpr}
-    alt="Descrição da imagem"
-    width="280"
-    height="60"
-    className="inline-block mt-5 "
-  />
-
+   
  
 
 
-                          <h1 className="text-3xl md:text-4xl font-bold mb-4 mt-3 text-yellow-300 text-roboto mr-3">
+                          <h1 className="text-3xl md:text-4xl font-bold mb-4  text-yellow-300 text-roboto mr-3">
                               CarCara-DAQ: Data Acquisition Test with Advanced Technologies
                           </h1>
                           <p className="text-base md:text-lg text-gray mb-4 max-w-lg">
@@ -71,7 +105,7 @@ const Home: React.FC = () => {
 
 
                   <img 
-  className="w-full max-w-md md:max-w-lg h-[550px] md:h-[620px] object-cover px-3"
+  className="w-full max-w-md md:max-w-lg h-[550px] md:h-[620px] object-cover mt-1 px-3"
   src={yolo} 
   
   alt="Descrição do GIF"
@@ -82,44 +116,13 @@ const Home: React.FC = () => {
               </div>
           </div>
 
-          
-          {/* Sessão CarCara-360DAQ */}
-<div className="bg-zinc-950 flex-grow flex justify-center px-4">
-    <div className="w-full max-w-5xl flex mt-3  flex-col md:flex-row items-center md:items-start px-3 text-white">
-
-        
-    <div className="  md:w-1/2 ml-3 text-center md:text-left flex justify-center md:justify-start md:items-center h-full">
-            <div className="flex flex-col items-center md:items-start">
-                <h1 className="text-2xl md:text-3xl font-bold mb-4 text-yellow-300 text-roboto mr-3">
-                    CarCara-DAQ: A Multi-Sensor Dataset for Real-Time Automotive Data Acquisition
-                </h1>
-                <Link to="/Intermediary">
-                    <button className=" bg-yellow-300 text-zinc-900 hover:bg-yellow-100 text-base md:text-lg font-bold py-2 px-4 md:px-6 rounded-full transition duration-300 text-roboto mb-6">
-                        Download Here
-                    </button>
-                </Link>
-            </div>
-        </div>
-    <div className="md:w-1/2 flex justify-center items-center mr-3">
-    <img 
-  className="w-full max-w-md md:max-w-lg h-[300px] md:h-[415px] object-cover"
-  src={heat} 
-  alt="Descrição do GIF"
-/>
-        </div>
-
-
-
-      
-    </div>
-</div>
 
 
 
         
 
             {/* Sessão Examples */}
-            <div className="bg-zinc-900 text-white py-5 px-4">
+            <div className="bg-zinc-900 text-white py-1 px-4">
                 <div className="max-w-5xl mx-auto text-center">
                     <h2 className="text-3xl md:text-4xl font-bold mb-4 text-yellow-300 text-roboto">Examples</h2>
                     <div className="grid grid-cols-1 md:grid-cols-1 gap-4">
