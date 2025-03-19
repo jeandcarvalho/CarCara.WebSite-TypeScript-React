99eca4f143b1d82ce82aa46e5c2b278a431235c9
Jean Carvalho <jeanbrandaocarvalho@hotmail.com>
2025-03-19 09:47:38 -0300

    Updates v4.2

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -210,7 +210,7 @@ const Home: React.FC = () => {
             </div>
 
             {/* Sessão Mapa e Estados Visitados */}
-            <div className="bg-zinc-950 text-white py-10 px-4">
+            <div className="bg-zinc-950 text-white py-5 px-4">
                 <div className="max-w-5xl mx-auto text-center">
                     <p className="text-yellow-400 text-xs md:text-sm py-3">Visited States:
                         <span className="text-gray-300">
@@ -219,7 +219,17 @@ const Home: React.FC = () => {
                     </p>
                     <MapComponent states={states} />
                 </div>
+
+                <div className="flex justify-center items-center mt-5">
+  <button
+    onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+    className="bg-gray-700 text-white hover:bg-gray-600 text-base md:text-lg font-bold py-3 px-5 rounded-full transition duration-300 text-roboto"
+  >
+    ↑ Back to Top
+  </button>
+</div>
             </div>
+            
 
             <Footer />
         </div>
