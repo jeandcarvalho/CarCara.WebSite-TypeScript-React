097e67b685b414df26115cd11fab86c37ff84382
Jean Carvalho <jeanbrandaocarvalho@hotmail.com>
2025-03-22 10:22:47 -0300

    Updates v4.2

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,18 +1,32 @@
-const Footer: React.FC = () => (
-  <footer className="bg-zinc-900 text-white py-2">
-    <div className="container mx-auto text-center">
-      <p className="text-sm px-2">&copy; 2024 GSA. All rights reserved.</p>   
-    </div>
-  </footer>
-);
-export default Footer;
+import { Link } from "react-router-dom";
 
-/*
-import { Link } from 'react-router-dom';
- <nav className="flex justify-center mt-2 flex-wrap">
-        <Link to="/" className="text-zinc-400 hover:text-white mx-2">Home</Link>
-        <Link to="/Search" className="text-zinc-400 hover:text-white mx-2">Acquisitions</Link>
-        <Link to="/About" className="text-zinc-400 hover:text-white mx-2">About</Link>
-        <Link to="/OurModels" className="text-zinc-400 hover:text-white mx-2">Our Models</Link>
-      </nav>
-*/
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-zinc-900 text-white py-4">
+      <div className="container mx-auto text-center">
+        {/* Links do site */}
+        <nav className="flex flex-wrap justify-center gap-4 mb-2">
+          <Link to="/" className="text-zinc-400 hover:text-white">Home</Link>
+          <Link to="/Intermediary" className="text-zinc-400 hover:text-white">Acquisitions</Link>
+          <Link to="/About" className="text-zinc-400 hover:text-white">About</Link>
+          <Link to="/OurModels" className="text-zinc-400 hover:text-white">Our Models</Link>
+        </nav>
+
+        {/* Links de contato */}
+        <div className="flex justify-center gap-4 mb-2">
+          <a href="https://www.instagram.com/gsa.pg/" target="_blank" rel="noopener noreferrer" className="text-zinc-400 hover:text-white">
+            Instagram
+          </a>
+          <a href="https://www.linkedin.com/company/gsapg" target="_blank" rel="noopener noreferrer" className="text-zinc-400 hover:text-white">
+            LinkedIn
+          </a>
+        </div>
+
+        {/* Direitos autorais */}
+
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
